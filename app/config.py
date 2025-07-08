2ee7d2a129b48e8d4073df4c184f5ad2a12b9580
Aayush Man Shrestha <aayush.m.shrestha@gmail.com>
2025-07-08 12:05:38 +0545

    feat: add support for more LLM providers

--- app/config.py
+++ app/config.py
@@ -8,5 +8,5 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 VECTORSTORE_DIR = "app/faiss_index"
 EMBEDDING_MODEL = "models/embedding-001"
-LLM_MODEL = "gemini-2.0-flash"
+LLM_PROVIDER = "Groq"
 MAX_HISTORY_SIZE = 2
